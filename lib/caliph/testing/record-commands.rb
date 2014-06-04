174ab2fcf2dadb890f2e56b73de2967218f48442
Evan Dorn <evan@lrdesign.com>
2014-06-03 17:54:09 -0700

    Remove ENV reference to Mattock

--- lib/caliph/testing/record-commands.rb
+++ lib/caliph/testing/record-commands.rb
@@ -15,7 +15,7 @@ module Caliph
       attr_accessor :command_recording_path
 
       def command_recording_path
-        @command_recording_path ||= ENV['MATTOCK_CMDREC']
+        @command_recording_path ||= ENV['CALIPH_CMDREC']
       end
 
       def emit_recording
@@ -23,7 +23,7 @@ module Caliph
         if command_recording_path
           io = File.open(command_recording_path, "w")
         else
-          io.puts "Set MATTOCK_CMDREC to write to a path"
+          io.puts "Set CALIPH_CMDREC to write to a path"
         end
         @@commands.each do |pair|
           io.puts "[/#{pair[0]}/, #{[pair[1].exit_code, pair[1].streams].inspect}]"
